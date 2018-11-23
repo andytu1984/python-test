be897e953f8f18c09220d1e0611209ef4fab479d
andytu1984 <sap_tuchong@aliyun.com>
2018-11-23 10:28:43 +0800

    python program update

---
+++ package01/andytest01.py
@@ -0,0 +1,12 @@
+'''
+Created on Oct 29, 2018
+
+@author: I308747
+'''
+a = 'andy' + 'test'
+print(a)
+
+b = ['andytu', 35, 170, 'yuan', 27, 162, 'xiaoyi', 2, 90]
+b[1] += 1
+print(b[1])
+print(len(b))
